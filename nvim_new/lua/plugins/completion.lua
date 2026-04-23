858b0902bc1ed4e9e0cd1f17639565d13a90142d
tokoHerold <wombatwheelie@gmail.com>
2026-04-23 05:08:40 -0700

    Fix code completion buggin on multiple <tab> inputs

--- nvim_new/lua/plugins/completion.lua
+++ nvim_new/lua/plugins/completion.lua
@@ -3,20 +3,30 @@ local remap = vim.api.nvim_set_keymap
 -- Setup autopairs
 local npairs = require('nvim-autopairs')
 npairs.setup({ map_bs = false, map_cr = false })
-
 -- Setup suround
 require("nvim-surround").setup({})
-
 -- Setup completion
 vim.g.coq_settings = { keymap = { recommended = false } }
 
 -- these mappings are coq recommended mappings unrelated to nvim-autopairs
-remap('i', '<cr>',
-	[[pumvisible() ? (complete_info().selected == -1 ?]] ..
-	[["<c-e><cr>" :]] .. -- Selection shown but nothing selected
-	[["<c-y>") :]] ..     -- Selection shown and something selected
-	[["<cr>"]],          -- Selection hidden
-	{ expr = true, noremap = true })
+_G.MUtils= {}
+MUtils.CR = function()
+  if vim.fn.pumvisible() ~= 0 then
+    if vim.fn.complete_info({ 'selected' }).selected ~= -1 then
+      return npairs.esc('<c-y>')
+    else
+      return npairs.esc('<c-e>') .. npairs.autopairs_cr()
+    end
+  else
+    return npairs.autopairs_cr()
+  end
+end
+remap('i', '<cr>', 'v:lua.MUtils.CR()', { expr = true, noremap = true })
+-- 	[["<c-e><cr>" :]] .. -- Selection shown but nothing selected
+-- 	[["<c-y>") :]] ..     -- Selection shown and something selected
+-- 	[[require('nvim-autopairs').autopairs_cr()]],          -- Selection hidden
+-- 	{ expr = true, noremap = true })
+--
 remap('i', '<tab>',
 	[[pumvisible() ? (complete_info().selected == -1 ?]] ..
 	[["<c-e><tab>" :]] .. -- Selection shown but nothing selected
